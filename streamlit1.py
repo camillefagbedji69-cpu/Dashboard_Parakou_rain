3c49d652bf37030d8f8b698cb3e8163f1385e184
camillefagbedji69-cpu <camillefagbedji69@gmail.com>
2025-09-09 22:18:20 +0200

    streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -2,10 +2,19 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("Parakou_Daily.csv", sep = ",", skiprows = 13)
-df['Date'] = pd.to_datetime(df['YEAR'].astype(str) + df['DOY'].astype(str), format='%Y%j')
+df = pd.read_csv("Parakou_Daily.csv", sep = ",", skiprows = 13)  ##importation de la base de données 
+
+df['Date'] = pd.to_datetime(df['YEAR'].astype(str) + df['DOY'].astype(str), format='%Y%j') ## conversion en date 
+df = df.rename(columns={
+    "T2M_MAX":"Tmax",
+    "T2M_MIN":"Tmin",
+    "PRECTOTCORR":"Rain",
+    "ALLSKY_SFC_SW_DWN":"Radiation",
+    "QV2M":"QV"
+})
 
 st.title("Dashboard de la pluviométrie dans la commune de Parakou (Bénin)")
 
-fig = px.line(df, x="Date", y="mean", title="Évolution du NDVI")
+fig = px.line(df, x="Date", y="Rain", title="Évolution de la pluviométrie")
 st.plotly_chart(fig)
+
