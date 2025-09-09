05977d2c6d47cfbe773f41712166db89b9dfc9f8
camillefagbedji69-cpu <camillefagbedji69@gmail.com>
2025-09-09 22:08:02 +0200

    Add files via upload

---
+++ streamlit1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv(r"c:\Users\ACER\Documents\Projet IA\Datasets\Parakou_Daily.csv", sep = ",", skiprows = 13)
+df['Date'] = pd.to_datetime(df['YEAR'].astype(str) + df['DOY'].astype(str), format='%Y%j')
+
+st.title("Dashboard de la pluviométrie dans la commune de Parakou (Bénin)")
+
+fig = px.line(df, x="Date", y="mean", title="Évolution du NDVI")
+st.plotly_chart(fig)
